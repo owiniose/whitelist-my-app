f53c00dbb5b3442fb9a7d64110535ebeb5774182
owiniose <98717429+owiniose@users.noreply.github.com>
2022-02-15 02:35:25 +0100

    Add files via upload

---
+++ my-app/constants/index.js
@@ -0,0 +1,66 @@
+export const abi = [
+	{
+		"inputs": [],
+		"name": "addAddressToWhitelist",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint8",
+				"name": "_maxWhitelistedAddresses",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "maxWhitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "numWhitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "whitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+export const WHITELIST_CONTRACT_ADDRESS = "0x191e443bC9B6edC7e1c6CaE9fBC54b3b8D83C7ec";
